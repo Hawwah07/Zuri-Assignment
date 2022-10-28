10903a0315a9f41cb3f78bfe2a02e0ae106fa2f5
Hawwah07 <114662329+Hawwah07@users.noreply.github.com>
2022-10-28 02:42:34 +0100

    Create the buttons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -3,24 +3,24 @@ import React from "react";
 export default function Buttons() {
     return (
        <div className="btn">
-        <a id="twitter" target="_blank" href="twitterlinkwhenimakeit">Twitter Link</a>
-        <a target="_blank" id="btn__zuri" href="https://training.zuri.team/">Zuri Team</a>
+        <a id="twitter" target="_blank" href="twitterlinkwhenimakeit"><p>Twitter Link</p></a>
+        <a target="_blank" id="btn__zuri" href="https://training.zuri.team/"><p>Zuri Team</p></a>
         
         <a target="_blank" id="books" href="http://books.zuri.team ">
-            <h3>Zuri Books</h3>
-            <sub>You can get books about design and coding here</sub>
+            <p>Zuri Books</p>
+            <small>You can get books about design and coding here</small>
             </a>
         <a target="_blank" id="book__python" href="https://books.zuri.team/python-for-beginners?ref_id=Hawwah">
-            <h3>Python Books</h3>
-            <sub>You can get Python book for beginners and start you coding journey today</sub>
+            <p>Python Books</p>
+            <small>You can get Python book for beginners and start you coding journey today</small>
             </a>
         <a target="_blank" id="pitch" href="https://background.zuri.team">
-            <h3>Background Check for Coders</h3>
-            <sub>Background Checks Today, Fast and Accurate</sub>
+            <p>Background Check for Coders</p>
+            <small>Background Checks Today, Fast and Accurate</small>
             </a>
         <a target="_blank" id="book__design" href="https://books.zuri.team/design-rules">
-            <h3>Design Book</h3>
-            <sub>Click and get Free Design Book Offered by Zuri</sub>
+            <p>Design Book</p>
+            <small>Click and get Free Design Book Offered by Zuri</small>
             </a>
         
        </div> 

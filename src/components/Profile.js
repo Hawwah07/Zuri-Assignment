b97192ca3f2f7f7554e238aea55b378fc66f8072
Hawwah07 <114662329+Hawwah07@users.noreply.github.com>
2022-10-28 02:03:48 +0100

    Modify profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,11 +4,11 @@ import Iconarrow from "../images/Iconarrow.png"
 
 export default function Profile() {
     return (
-        <div>
+        <div className="profile">
             <img id="profile__img" src={profileImage}/>
             <h3>Annette Black</h3>
             {/* <p id="slack">Hawwah</p> */}
-            <img src={Iconarrow}/>
+            <img src={Iconarrow} className="arrow"/>
         </div>
     )
 }

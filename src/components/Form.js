5ea0950fa4230937e1260480579751a13a14e118
Hawwah07 <114662329+Hawwah07@users.noreply.github.com>
2022-11-05 00:53:47 +0100

    style: Updated the style.css

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,58 @@
 import React from "react";
 
 export default function Form() {
-    return (
-        <form className="form-container">
-            <div className="form-name">
-                <div className="first-name">
+  return (
+    <div className="form-container">
+      <form>
+        <div className="name-div">
+          <div className="first-name-div">
             <label className="first-name-label">First Name</label>
-            <input type="text" id="first_name" placeholder="Enter your first name" />
-            </div>
-            <div className="last-name">
+            <input
+              type="text"
+              id="first_name"
+              placeholder="Enter your first name"
+            />
+          </div>
+
+          <div className="last-name-div">
             <label className="last-name-label">Last Name</label>
-            <input type="text" height="" width="" id="last_name" placeholder="Enter your last name" />
-            </div>
-            
-            </div>
-            <label>Email
-            <input type="text" height="" width="" id="email" placeholder="yourname@email.com" />
-            </label>
-            
-            <label>Message
-            <textarea id="message" placeholder="Send me a message and I'll reply you as soon as possible..."></textarea>
-            </label>
-            <label className="checkbox-label">   
-        <input type="checkbox" id="checkbox" />
-        You agree to providing your data to Hawwah who may contact you.</label>
-        <button type="button" id="btn__submit">Send message</button>
-        </form>
-        
-    )
+            <input
+              type="text"
+              height=""
+              width=""
+              id="last_name"
+              placeholder="Enter your last name"
+            />
+          </div>
+        </div>
+
+        <div class="email-div">
+          <label>Email</label>
+          <input
+            type="text"
+            height=""
+            width=""
+            id="email"
+            placeholder="yourname@email.com"
+          />
+        </div>
+        <div class="message-div">
+          <label>Message </label>
+          <textarea
+            id="message"
+            placeholder="Send me a message and I'll reply you as soon as possible..."
+          ></textarea>
+        </div>
+        <div className="box-div">
+          <label className="checkbox-label">
+            <input type="checkbox" id="checkbox" />
+            You agree to providing your data to Hawwah who may contact you.
+          </label>
+        </div>
+        <button type="button" id="btn__submit">
+          Send message
+        </button>
+      </form>
+    </div>
+  );
 }

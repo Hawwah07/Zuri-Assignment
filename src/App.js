24a888e168e651803e0290147a70af80676bdaae
Hawwah07 <114662329+Hawwah07@users.noreply.github.com>
2022-11-04 16:55:35 +0100

    style: Updated the style.css

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default function App() {
     return (
-        <div>
+        <div className="app-container">
         <Router>
         <Routes>
             <Route path="/Zuri-Assignment" element={<Home />} />

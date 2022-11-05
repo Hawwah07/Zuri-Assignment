6c201a9aacf2ee288ec3a2bbe05bb3eed01acf05
Hawwah07 <114662329+Hawwah07@users.noreply.github.com>
2022-11-05 16:36:41 +0100

    feat: Added input validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,17 @@
 import { click } from "@testing-library/user-event/dist/click";
 import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function Form() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+
   return (
     <div className="form-container">
-      <form action="/new.php" onSubmit={click}>
+      <form action="/new.php" onSubmit={handleSubmit(onSubmit)}>
         <div className="name-div">
           <div className="first-name-div">
             <label className="first-name-label">First Name</label>
@@ -12,8 +19,10 @@ export default function Form() {
               type="text"
               id="first_name"
               placeholder="Enter your first name"
-              required="required"
+              // required="required"
+              {...register("firstName", { required: true, minLength: 5 })}
             />
+            {errors.firstName && <p className="errortext">Please check the First Name</p>}
           </div>
 
           <div className="last-name-div">
@@ -24,8 +33,10 @@ export default function Form() {
               width=""
               id="last_name"
               placeholder="Enter your last name"
-              required
+              {...register("lastName",  { required: true, minLength: 5 })}
+              // required
             />
+            {errors.lastName && <p className="errortext">Please check the Last Name</p>}
           </div>
         </div>
 
@@ -37,22 +48,30 @@ export default function Form() {
             width=""
             id="email"
             placeholder="yourname@email.com"
-            required
+            // required
+            {...register("email", { required: true, pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ })}
           />
+          {errors.email && <p className="errortext">Please check the Email</p>}
         </div>
         <div class="message-div">
           <label>Message </label>
           <textarea
             id="message"
             placeholder="Send me a message and I'll reply you as soon as possible..."
-            required
+            // required
+            {...register("message", { required: true, minLength: 10 })}
           ></textarea>
+          {errors.message && <p className="errortext">Please enter a message.</p>}
         </div>
         <div className="box-div">
           <label className="checkbox-label">
-            <input type="checkbox" id="checkbox" required />
+            <input type="checkbox" id="checkbox"
+            {...register("checkbox", { required: true })}
+            //  required 
+            />
             You agree to providing your data to Hawwah who may contact you.
           </label>
+            {errors.message && <p className="errortext">Please check this box.</p>}
         </div>
         <button type="submit" id="btn__submit" value="submit">
           Send message
